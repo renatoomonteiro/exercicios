3f286105b1ae23105d382b120deaebe307c0d661
Monteiro <Monteiro@DESKTOP-2DTKVHQ>
2020-12-30 17:13:02 -0300

    Inseri a classe desafio

---
+++ exercicios/src/arrays/Desafio.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Desafio {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Quantas notas");
+		
+		int qtdeNotas = entrada.nextInt();
+		System.out.println(qtdeNotas);
+
+		entrada.close();
+	}
+
+}
