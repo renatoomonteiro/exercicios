bfa48699cff4661f7f69fbd3e45aa977bb4527ae
Monteiro <Monteiro@DESKTOP-2DTKVHQ>
2020-12-30 17:49:48 -0300

    Alterei a classe Foreach

--- exercicios/src/arrays/Desafio.java
+++ exercicios/src/arrays/Desafio.java
@@ -1,16 +1,32 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Desafio {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 
-		System.out.println("Quantas notas");
+		System.out.println("Quantas notas: ");
 		
 		int qtdeNotas = entrada.nextInt();
-		System.out.println(qtdeNotas);
-
+		
+		double [] notas = new double [qtdeNotas];
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Informe a nota " + (i + 1) + ": ");
+			notas[i] =  entrada.nextDouble();
+		}
+		
+		System.out.println(Arrays.toString(notas));
+			
+		double total = 0;
+		for(double nota: notas) {
+			total+=nota;
+		}
+		
+		double media = total/notas.length;
+		System.out.println("A média é " + media + "!");
 		entrada.close();
 	}
 
--- exercicios/src/arrays/Foreach.java
+++ exercicios/src/arrays/Foreach.java
@@ -2,6 +2,7 @@ package arrays;
 
 public class Foreach {
 	public static void main(String[] args) {
+		
 		double[] notas = {9.9,8.7,7.2,9.4};
 
 		for (int i = 0; i < notas.length; i++) {
