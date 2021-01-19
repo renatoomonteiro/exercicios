7e7390e83b5a6cbd25fe5cb20768ff3298e8ea8c
drare <drare@DESKTOP-73PJKRR>
2021-01-19 09:55:10 -0300

    Criei a classe ConjuntoComportado

--- exercicios/src/colecoes/ConjuntoComportado.java
+++ exercicios/src/colecoes/ConjuntoComportado.java
@@ -6,7 +6,13 @@ import java.util.Set;
 public class ConjuntoComportado {
 	public static void main(String[] args) {
 		
-		Set lista = new HashSet();
+		Set<String> lista = new HashSet<String>(); // <String> significa que é uma lista que aceitará valores do tipo string, 
+		//(Entre " ")
+		lista.add("Ana");
+		lista.add("Carlos");
+		lista.add("Luca");
+		lista.add("Pedro");
+		
 		
 		
 		
