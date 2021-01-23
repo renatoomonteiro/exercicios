803e76deb602b86bb676b3fd8b0ba590c330295d
drare <drare@DESKTOP-73PJKRR>
2021-01-23 07:44:00 -0300

    Criei a classe Mapa

---
+++ exercicios/src/colecoes/Mapa.java
@@ -0,0 +1,18 @@
+package colecoes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapa {
+
+	public static void main(String[] args) {
+		//Mapa: Estrutura no modelo "chave":"valor"
+		
+		Map<Integer, String> usuarios = new HashMap<Integer, String>();
+		
+		
+		
+
+	}
+
+}
