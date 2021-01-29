efbc6ea76b950ba92c69c495197efef0a6d717f0
drare <drare@DESKTOP-73PJKRR>
2021-01-29 10:13:53 -0300

    Editei as classes Carro, Carro Teste e Motor

--- exercicios/src/oo/composicao/Motor.java
+++ exercicios/src/oo/composicao/Motor.java
@@ -1,5 +1,23 @@
 package oo.composicao;
 
 public class Motor {
+
+	//boolean ligado = false;
+	
+	Carro carro;
 	double fatorInjecao = 1;
+	public boolean ligado= false;
+	
+	Motor(Carro carro){
+		this.carro = carro;
+	}
+	
+	int giros() {
+
+		if(!ligado) {
+			return 0;
+		} else {
+			return (int) Math.round(fatorInjecao*3000);
+		}
+	}
 }
