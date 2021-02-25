a556b4ee93d781c4f4906908465efffbdc123b45
drare <drare@DESKTOP-73PJKRR>
2021-02-25 10:18:24 -0300

    Editei as classes Heroi e Jogo

--- exercicios/src/oo/heranca/Heroi.java
+++ exercicios/src/oo/heranca/Heroi.java
@@ -3,4 +3,13 @@ package oo.heranca;
 public class Heroi extends Jogador{ // Classe Heroi que herda as características da classe Jogador
 // Através da palavra reservada "extends".
 
+	boolean atacar(Jogador oponente) { // Método atacar que irá receber um parâmetro oponente para calcular a distância entre os jogadores 
+		boolean ataque1 = super.atacar(oponente); // Método da classe pai, sendo utilizado através da palavra super
+		boolean ataque2 = super.atacar(oponente);
+		boolean ataque3 = super.atacar(oponente);
+		
+		//Retorne o ataque 1, 2 ou 3.
+		return ataque1 || ataque2 || ataque3;// Pipeline  ( || ) significa OU.
+	}	
+	
 }
--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -9,15 +9,15 @@ public class Jogo {
 		//Classe pai -> Jogador
 		//Classes filhas -> Monstro e Herói
 		
-		Monstro j1 = new Monstro(); // Instanciando um novo Jogador
-		j1.x = 10; // O jogador 1 andou 10 posições na variável x
-		j1.y = 10; // O jogador 1 andou 20 posições na variável y
+		Monstro monstro = new Monstro(); // Instanciando um novo Jogador
+		monstro.x = 10; // O jogador 1 andou 10 posições na variável x
+		monstro.y = 10; // O jogador 1 andou 20 posições na variável y
 		
 		
 		// Utilizando a classe Heroi, que herda as características da classe Jogador.
-		Heroi j2 = new Heroi(); // Instanciando um novo Jogador
-		j2.x = 10; // O jogador 1 andou 10 posições na variável x
-		j2.y = 11; // O jogador 1 andou 20 posições na variável y
+		Heroi heroi = new Heroi(); // Instanciando um novo Jogador
+		heroi.x = 10; // O jogador 1 andou 10 posições na variável x
+		heroi.y = 11; // O jogador 1 andou 20 posições na variável y
 		
 //		// Chamando o método passando parâmetro do tipo String
 //		j1.andar("norte"); // Jogador 1 andou na direção norte uma vez
@@ -37,13 +37,20 @@ public class Jogo {
 //		j1.andar(Direcao.NORTE); // Jogador 1 andou na direção norte uma vez
 //		j1.andar(Direcao.LESTE); // Jogador 1 andou na direção norte uma vez
 		
-		System.out.println(j1.vida); // Exibição de quantas vezes o jogador 1 andou na coordenada y
-		System.out.println(j2.vida); // Exibição de quantas vezes o jogador 1 andou na coordenada x
+		System.out.println("Monstro tem => " + monstro.vida); // Exibição de quantas vezes o jogador 1 andou na coordenada y
+		System.out.println("Heroi tem => " + heroi.vida); // Exibição de quantas vezes o jogador 1 andou na coordenada x
 		
-		j1.atacar(j2);// Método atacar da classe Jogador com o parâmetro j2, jogador 2.
-		j2.atacar(j1);// Jogador 2 ataca o jogador 1
+		monstro.atacar(heroi);// Método atacar da classe Jogador com o parâmetro j2, jogador 2.
+		heroi.atacar(monstro);// Jogador 2 ataca o jogador 1
 		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		monstro.andar(Direcao.NORTE);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		System.out.println("Monstro tem => " + monstro.vida);
+		System.out.println("Heroi tem => " + heroi.vida);
 	}
 }
