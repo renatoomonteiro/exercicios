1a9775de6825c1119d7f08819e402e884d2d6934
Monteiro <Monteiro@DESKTOP-2DTKVHQ>
2021-03-19 18:10:03 -0300

    Editei as classe Carro para o desafio

--- exercicios/src/oo/heranca/desafio/Carro.java
+++ exercicios/src/oo/heranca/desafio/Carro.java
@@ -1,12 +1,28 @@
 package oo.heranca.desafio;
 
 public class Carro {
-
-	int velocidadeAtual;
+	/*
+	 * Desafio da aula 152
+	 * A classe pai já não possui mais o construtor padrão, será necessário dar manutenção nas classes filhas:
+	 * Ferrari e Fusca
+	 * */
+	
+	final int VELOCIDADE_MAXIMA;
+	int velocidadeAtual, delta=5;
+	
+	Carro (int velocidadeMaxima){
+		VELOCIDADE_MAXIMA = velocidadeMaxima;
+	}
 	
 	void acelerar()  {
-		velocidadeAtual+=5;		
+		if(velocidadeAtual + delta > VELOCIDADE_MAXIMA) {
+			velocidadeAtual = VELOCIDADE_MAXIMA;
+		}else {
+			velocidadeAtual +=delta;
+		}
 	}
+	
+	
 	void frear() {
 		if (velocidadeAtual >= 5) {
 		velocidadeAtual -= 5;
