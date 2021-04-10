496d29a5db9f71ee97c73fb82db95fbef0240098
Monteiro <Monteiro@DESKTOP-2DTKVHQ>
2021-04-10 16:57:19 -0300

    Editei as classes Pessoa e PessoaTeste

--- exercicios/src/oo/encapsulamento/Pessoa.java
+++ exercicios/src/oo/encapsulamento/Pessoa.java
@@ -1,15 +1,63 @@
 package oo.encapsulamento;
 
 public class Pessoa {
-	private int idade; // Só que pode ler e editar essa variável será a classe Pessoa
+	
+	private String nome, sobrenome;
+	private int idade; // Só quem pode ler e editar essa variável será a classe Pessoa
 	
 	// Métodos Getters Setter
 	
-	public Pessoa(int idade) {
-		this.idade = idade;
+	public Pessoa(String nome, String sobrenome, int idade) {
+		setNome(nome);
+		setSobrenome(sobrenome);
+		setIdade(idade);		
 	}
 	
-	public int lerIdade() {
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getNomeCompleto(){
+		return getNome() + " " + getSobrenome();
+				
+	}
+	//Famoso método Getter 
+	//antigo método lerIdade
+	public int getIdade() {
 		return idade;
+	}
+	
+	//Famoso método Setter (altera)
+	//Antigo método alterarIdade
+	public void setIdade(int novaIdade) { 
+		novaIdade = Math.abs(novaIdade);
+		if(novaIdade >=0 && novaIdade <=120) {
+			this.idade = novaIdade;	
+		}
+	}
+	
+	//Sobrescrita do método toString
+	@Override
+	public String toString() {
+		return "Olá! Eu sou o " + getNome()
+		+ " e tenho " + getIdade() + " anos."; 
+		
 	}
 }
